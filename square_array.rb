6622e8db297edeecd4f17504658a7be32d2ba9b9
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-10 02:10:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
-def square_array(array)
-  my_array = []
-  array.each do |n|
-    square = n ** 2
-    my_array << square
-  end
-  my_array
-end
+#def square_array(array)
+ # my_array = []
+  #array.each do |n|
+    #square = n ** 2
+   # my_array << square
+ # end
+ # my_array
+#end
+
